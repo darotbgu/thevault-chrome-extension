efe03ac8abd79dbec2248f6c243e6ec8bbf64e20
rotem <darot@post.bgu.ac.il>
2020-04-20 09:19:30 +0300

    working on login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AppComponent} from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {PasswordModule} from 'primeng/password';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
